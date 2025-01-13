dc4648cad5abc90238ba3ac1637a81b7f30b2d84
Swastik Makwana <mswastik2004@gmail.com>
2025-01-13 21:31:34 +0530

    Add P1 from https://github.com/MakwanaSwastik/P1_21012011046

---
+++ P1/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var a: Int = 22
+    var b = 1.5F // Float
+    var c: Char = 'T'
+    var d: String = "SWASTIK MAKWANA"
+    var e: Boolean = false
+    var f: Double = 1.34374
+    val g: Long = 21012011046
+    val h: Short = -2
+    val i: Byte = 127
+
+    println("Integer Value: $a")
+    println("Float Value: $b")
+    println("Char Value: $c")
+    println("String Value: $d")
+    println("Boolean Value: $e")
+    println("Double Value: $f")
+    println("Long Value: $g")
+    println("Short Value: $h")
+    println("Byte Value: $i")
+}
---
+++ P1/src/main/kotlin/Q2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var int_val: Int = 10
+    var new_value: Double = int_val.toDouble()
+    println("21012011046")
+    println("Integer Value: $int_val \nDouble From Integer: $new_value")
+    var string: String = "10"
+    var new_string: Int = string.toInt()
+    var dou_string: Double = string.toDouble()
+    var new: Double = dou_string + 1.12
+    println("String Value: $string \nInteger Value1: $new_string \nDouble Value: $dou_string")
+    println("New Double Value: $new")
+}
